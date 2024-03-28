ca29188daa4df7ca64dd41c952d36835ffd735b3
sivarajagopalans <sivarajagopalans@gmail.com>
2024-03-28 20:37:01 +0530

    feat: add github profile in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,14 +7,15 @@ function NavBarComponent() {
         <div > 
       <Navbar expand="lg" className="fixed-top text-white" style={{background: "rgb(25, 25, 36)"}}>
       <Container className='text-white'>
-        <Navbar.Brand href="#home"><h2 className='text-white'><span><i className="bi bi-person-circle"></i> Portfolio</span></h2></Navbar.Brand>
+        <Navbar.Brand href="#home"><h3 className='text-white'><span><i className="bi bi-person-circle"></i> Portfolio</span></h3></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" className='navbar-dark'/>
         <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
-          <Nav className='h4'>
+          <Nav className='h5'>
             <Nav.Link href="#about" className='text-white'>About</Nav.Link>
             <Nav.Link href="#skills" className='text-white'>Skills</Nav.Link>
             <Nav.Link href="#projects" className='text-white'>Projects</Nav.Link>
-            {/* <Nav.Link href="#education" className='text-white'>Education</Nav.Link> */}
+            <Nav.Link href="https://github.com/sivarajagopalans" target='_blank' className=' '
+            style={{border:"1.8px solid rgb(133, 76, 230)", borderRadius:"15px",padding:"10px",paddingTop:"10px",display:"flex", justifyContent:"center", alignItems:"center",flexWrap:"wrap",color: "rgb(133, 76, 230)"}}>GitHub Profile</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
